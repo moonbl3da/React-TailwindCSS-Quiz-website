b3e069dd685b28c1f4998a6e7481ac550fc67c42
moonbl3da <dominikkikina68@gmail.com>
2022-11-04 17:29:21 +0100

    Header created

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rofile" element={<Profile />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'; 
+
+export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    function pathMatchRoute(route)
+    {
+        if (route === location.pathname)
+        {
+            return true;
+        }
+    }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0">
+        <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+            <div>
+                <img src="NEM MÜKÖDIK ROHADJON MEG" alt="logo" className='h-5 cursor-pointer' onClick={() => navigate("/")}/>
+            </div>
+            <div>
+                <ul className="flex space-x-10">
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-600 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-blue-500 "}`} onClick={() => navigate("/")}>Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-600 border-b-[3px] border-b-transparent ${pathMatchRoute("/quiz") && "text-black border-b-blue-500 "}`} onClick={() => navigate("/quiz")}>Quiz</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-600 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-blue-500 "}`} onClick={() => navigate("/sign-in")}>Sign In</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
